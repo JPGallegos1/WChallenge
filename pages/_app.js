ea93c928cd7fee159d975cd080a18f3c0fcfca62
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-08-27 07:19:46 -0300

    add: customTheme

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import Header from "../components/layout/header";
+import { CustomTheme } from "../styles/theme";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={CustomTheme}>
       <CSSReset />
+      <Header />
       <Component {...pageProps} />
     </ThemeProvider>
   );
