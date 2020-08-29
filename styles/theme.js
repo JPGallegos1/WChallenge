42571ee3d1497dab39e9042607f495ff14ad3be2
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-08-29 01:14:54 -0300

    fix: breakpoints for responsive design

--- styles/theme.js
+++ styles/theme.js
@@ -1,10 +1,9 @@
 import { theme } from "@chakra-ui/core";
 
-const breakpoints = ["300px", "768px", "1024px", "1440px"];
+const breakpoints = ["300px", "768px", "1024px"];
 breakpoints.sm = breakpoints[0];
 breakpoints.md = breakpoints[1];
 breakpoints.lg = breakpoints[2];
-breakpoints.xl = breakpoints[3];
 
 export const CustomTheme = {
   ...theme,
