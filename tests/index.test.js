0fe0525ab12cba5a4df03f50aff25eecb21f8708
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-08-30 17:49:12 -0300

    test: added some test for password validations

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,29 @@
-import { render, screen } from "@testing-library/react";
-import App from "../pages/index";
+import React from "react";
+import { render, fireEvent, act } from "@testing-library/react";
+import Login from "../pages/login";
 
-describe("App", () => {
-  it("renders without crashing", () => {
-    render(<App />);
-    expect(
-      screen.getByRole("heading", { name: "Welcome to Wollox Challenge" })
-    ).toBeInTheDocument();
+test("it should validate length", async () => {
+  const { findByRole, findByText, findByLabelText } = render(<Login />);
+  const input = await findByLabelText("Password");
+
+  await act(async () => {
+    fireEvent.input(input, { target: { value: "abcd" } });
+    fireEvent.submit(await findByRole("button"));
+
+    const error = await findByText("La contraseña debe tener 8 caracteres");
+    expect(error).toBeInTheDocument();
+  });
+});
+
+test("it should validate password", async () => {
+  const { findByRole, findByText, findByLabelText } = render(<Login />);
+  const input = await findByLabelText("Password");
+
+  await act(async () => {
+    fireEvent.input(input, { target: { value: "abcd" } });
+    fireEvent.submit(await findByRole("button"));
+
+    const error = await findByText(/contraseña/);
+    expect(error).toBeInTheDocument();
   });
 });
