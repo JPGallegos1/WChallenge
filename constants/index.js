f199a0efc18b9fa0efe43988ad9be60dcadc64e8
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-08-29 21:55:21 -0300

    add: constants to use with our environment variables

---
+++ constants/index.js
@@ -0,0 +1,3 @@
+export const API = process.env.NEXT_PUBLIC_BASE_URL;
+export const LOGIN_ENDPOINT = process.env.NEXT_PUBLIC_LOGIN_ENDPOINT;
+export const TECHS_ENDPOINT = process.env.NEXT_PUBLIC_TECHS_ENDPOINT;
