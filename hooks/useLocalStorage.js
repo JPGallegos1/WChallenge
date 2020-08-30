4465f06a1ccc53445a6ef6e7e0ebafabf5aa1638
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-08-29 21:55:57 -0300

    add: custom hook for localstorage

---
+++ hooks/useLocalStorage.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export function useLocalStorage (key, initialValue) {
+  const [user, setUser] = useState(() => {
+    try {
+      // Get key from ls
+      const item = localStorage.getItem(key);
+
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // if error
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Looking to save a value
+  const setSession = (value) => {
+    try {
+      // In case we need to store a function
+      // eslint-disable-next-line valid-typeof
+      const userToStore = typeof value === Function ? value(user) : value;
+
+      // save the value
+      setUser(userToStore);
+
+      // save to ls
+      localStorage.setItem(key, JSON.stringify(userToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { user, setSession };
+}
