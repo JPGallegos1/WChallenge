fb70a7eb9da7aeb7bf9b7497e562497fc28f7992
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-09-02 07:16:04 -0300

    add: Factory pattern for technologies

--- components/technologies/techCounter.js
+++ components/technologies/techCounter.js
@@ -1,10 +1,15 @@
-import { Text } from "@chakra-ui/core";
+import { Box } from "@chakra-ui/core";
+import Notification from "../notifications/notification";
 const TechCounter = ({ techs }) => {
   return (
     <>
       {techs.length === 1
-        ? <Text ml={5}>Total: {techs.length} tecnología</Text>
-        : <Text ml={5}>Total: {techs.length} tecnologías</Text>
+        ? <Box ml={5}>
+          <Notification text={`Hay solo ${techs.length} tecnología`} status="error" />
+        </Box>
+        : <Box ml={5}>
+          <Notification text={`Hay un total de ${techs.length} tecnologías`} status="success" />
+        </Box>
       }
     </>
   );
