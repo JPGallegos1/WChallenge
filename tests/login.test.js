b5ca17746f1ef62b73d82013f449aaa5535aff6d
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-09-01 22:56:10 -0300

    test: login page and password validations

---
+++ tests/login.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, fireEvent, act } from "@testing-library/react";
+import Login from "../pages/login";
+
+describe("User should be see errors after a try login", () => {
+  test("it should validate length", async () => {
+    const { findByRole, findByText, findByLabelText } = render(<Login />);
+    const input = await findByLabelText("Password");
+
+    await act(async () => {
+      fireEvent.input(input, { target: { value: "abcd" } });
+      fireEvent.submit(await findByRole("button"));
+
+      const error = await findByText("La contraseña debe tener 8 caracteres");
+      expect(error).toBeInTheDocument();
+    });
+  });
+
+  test("it should validate password", async () => {
+    const { findByRole, findByText, findByLabelText } = render(<Login />);
+    const input = await findByLabelText("Password");
+
+    await act(async () => {
+      fireEvent.input(input, { target: { value: "abcd" } });
+      fireEvent.submit(await findByRole("button"));
+
+      const error = await findByText(/contraseña/);
+      expect(error).toBeInTheDocument();
+    });
+  });
+});
