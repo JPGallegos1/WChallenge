f37c686f5ee25757bac449f626cc91928b84957c
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-09-01 22:55:32 -0300

    ref: refactor of the logic and UI

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,10 @@
 import { useSubmit } from "../hooks/useSubmit";
-import { Input } from "@chakra-ui/core";
 import { useForm } from "react-hook-form";
+import Layout from "../components/layout/layout";
+import InputLogin from "../components/login/inputLogin";
+import InputPass from "../components/login/inputPass";
+import Remember from "../components/login/remember";
+import SubmitForm from "../components/login/submitForm";
 
 const Login = () => {
   const { register, errors, handleSubmit } = useForm({
@@ -14,54 +18,19 @@ const Login = () => {
   const { onSubmit, loading, submitting } = useSubmit();
 
   return (
-    <div>
+    <Layout>
       {loading && (<div>Cargando...</div>)}
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <Input
-            type="email"
-            name="email"
-            id="email"
-            ref={register({ required: "Introduce un mail válido" })}
-          />
+        <InputLogin register={register} errors={errors} />
 
-          {errors.email && <div>{errors.email.message}</div>}
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <Input
-            type="password"
-            name="password"
-            id="password"
-            ref={register({
-              required: "Introduce una contraseña válida",
-              minLength: {
-                value: 8,
-                message: "La contraseña debe tener 8 caracteres"
-              },
-              validate: (value) => {
-                return (
-                  [/[0-9]/].every((pattern) => pattern.test(value))
-                ) || "La contraseña debe incluir números";
-              }
-            })}
-          />
-          {errors.password && <div>{errors.password.message}</div>}
-        </div>
-        <div>
-          <label htmlFor="terms">Desea ser recordado?</label>
-          <input
-            type="checkbox"
-            name="remember"
-            id="remember"
-            ref={register}
-          />
-        </div>
-        <Input type="submit" as="button" disable={submitting} value="Iniciar sesión" />
+        <InputPass register={register} errors={errors} />
+
+        <Remember register={register} />
+
+        <SubmitForm submitting={submitting} />
       </form>
-    </div>
+    </Layout>
   );
 };
 
--- pages/tecnologias.js
+++ pages/tecnologias.js
@@ -1,9 +1,11 @@
 import { useTechsContext } from "../context/techsContext/techsContext";
 import Layout from "../components/layout/layout";
-import Title from "../components/title";
-import TechItem from "../components/techitem";
-import { SimpleGrid, Input } from "@chakra-ui/core";
 import SkeletonItem from "../components/skeleton";
+import TechTitle from "../components/technologies/techTitle";
+import TechInput from "../components/technologies/techInput";
+import TechSelect from "../components/technologies/techSelect";
+import TechList from "../components/technologies/techList";
+import TechCounter from "../components/technologies/techCounter";
 
 const Tecnologias = () => {
   const { techs, loading, setSearchTech, setSortTech } = useTechsContext();
@@ -12,27 +14,16 @@ const Tecnologias = () => {
     <Layout as="section" flexDirection={{ sm: "column" }}>
       {loading && <SkeletonItem />}
 
-      <Title>¡Bienvenide a la lista de tecnologías!</Title>
-      <Layout>
-        <Input
-          type="text"
-          name="tech"
-          id="tech"
-          onChange={e => setSearchTech(e.target.value)}
-          w="20rem"
-          placeholder="Buscar tecnología..."
-          borderColor="gray.500"
-          margin="0 auto"
-        />
-        <select onChange={e => setSortTech(e.target.value)}>
-          <option value="ASC">Ascendente</option>
-          <option value="DESC">Descendente</option>
-        </select>
+      <TechTitle />
+      <Layout m="0 auto">
+        <TechInput setSearchTech={setSearchTech} />
+
+        <TechSelect setSortTech={setSortTech} />
       </Layout>
-      <SimpleGrid as="ul" minChildWidth="300px" spacing="40px" justify="space-between" align="center" w="100%" pt="5rem">
-        {techs.map(tech => <TechItem key={tech.tech} tech={tech} />)}
-      </SimpleGrid>
-      <p>{techs.length}</p>
+
+      <TechList techs={techs} />
+
+      <TechCounter techs={techs} />
     </Layout>
   );
 };
