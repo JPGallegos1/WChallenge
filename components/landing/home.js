6875ebc4ead16caaeb9d05c4e302eec6b7907084
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-09-02 07:23:29 -0300

    fix: issues with moduled found by vercel

--- components/landing/home.js
+++ components/landing/home.js
@@ -1,8 +1,8 @@
 import Welcome from "./welcome";
-import Technologies from "./Technologies";
+import Technologies from "./technologies";
 import Woloxers from "./woloxers/woloxers";
-import Benefits from "./Benefits";
-import Requirements from "./Requirements";
+import Benefits from "./benefits";
+import Requirements from "./requirements";
 const HomePage = () => {
   return (
     <>
